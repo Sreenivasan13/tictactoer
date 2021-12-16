d8fb2235418858a24c3ead31c61d095dcf47fbd0
Sreenivasan13 <69409059+Sreenivasan13@users.noreply.github.com>
2021-12-17 03:36:11 +0530

    Tictactoe in react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './TicTacToe.js';
+import TicTacToe from './TicTacToe.js';
+
+function App() {
+  return (
+    <div className="App">
+        <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
